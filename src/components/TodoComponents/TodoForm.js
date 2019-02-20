72a97198713263bdf897c781b7967004cb3b30c2
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-02-20 17:56:06 -0500

    Created form

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const TodoForm = props => {
+    return (
+        <div>
+            <form>
+                <input 
+                type="text"
+
+                 />
+                <button>Add Todo</button>
+                <button>Clear Completed</button>
+             </form>
+      </div>
+    )
+}
+
+export default TodoForm;
